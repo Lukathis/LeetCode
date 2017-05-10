c3a707056ab69052fcfb4805257dacc6e7a2f417
Lukathis <xuchi16@126.com>
2017-05-10 16:16:07 +0800

    modify two sum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,10 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5, 6};
         int target = 8;
         
-        int[] res = twoSum(nums, target);
+        int[] res = twoSumWithHashMap(nums, target);
         
         for (int i = 0; i < res.length; i++) {
             System.out.print(res[i] + " ");
@@ -12,6 +14,7 @@ public class TwoSum {
         
     }
     
+    /** 两重循环暴力求解 **/
     public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         int len = nums.length;
@@ -31,5 +34,17 @@ public class TwoSum {
             }
         }
         return result;
+    }
+    
+    /** 用HashMap求解效率更高 **/
+    public static int[] twoSumWithHashMap(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);        
+        } throw new IllegalArgumentException("No solution");
     }
 }
