8d0440a4789750902c20236f158f0d244c1dbac3
Lukathis <xuchi16@126.com>
2017-05-18 18:11:53 +0800

    5-18 qsort

--- src/sortAlgs/QuickSort.java
+++ src/sortAlgs/QuickSort.java
@@ -2,11 +2,39 @@ package sortAlgs;
 
 public class QuickSort {
     /**快速排序基本思路：
-     * 
+     * 选定一个pivot
+     * 实现partition方法，将a中的数根据pivot分开，较小的在左侧，较大的在右侧，并返回pivot的位置；
+     * 实现sort方法，递归调用pivot左右两侧分别sort，直至只剩下单一元素。
      * **/
     
     /** ONLY MODIFY BELOW THIS LINE**/
     public static void sort(int[] a) {
+        sort(a, 0, a.length - 1);
+    }
+    
+    public static void sort(int[] a, int lo, int hi) {
+        if (lo >= hi) return;
+        int k = partition(a, lo, hi);
+        sort(a, lo, k - 1);
+        sort(a, k + 1, hi);
+    }
+    
+    private static int partition(int[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        
+        while (true) {
+            while (less(a[++i], a[lo]))
+                if (i == hi) break;
+            
+            while (less(a[lo], a[--j]))
+                if (lo == j) break;
+            
+            if (j <= i) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
     }
     /** ONLY MODIFY ABOVE THIS LINE**/
     
